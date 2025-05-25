fcaca0a11119ac7d749bb9db5274ade43cfb09ce
Császár Norbert <csaszar.norbi@gmail.com>
2025-05-25 12:00:03 +0200

    VehicleBase class defined

--- src/main/java/org/example/softdevinternassignment/model/VehicleBase.java
+++ src/main/java/org/example/softdevinternassignment/model/VehicleBase.java
@@ -1,6 +1,29 @@
 package org.example.softdevinternassignment.model;
 
-public class VehicleBase {
-    int range;
+public abstract class VehicleBase {
+    private final int range;
+    private final int capacity;
+    private final int fuelConsumption;
 
+    public VehicleBase(int capacity, int range, int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+        if(capacity<=0){
+            throw new IllegalArgumentException("Capacity must be greater than 0");
+        }
+        if(range<=0){
+            throw new IllegalArgumentException("Range must be greater than 0");
+        }
+        this.capacity = capacity;
+        this.range = range;
+    }
+
+    public int getRange(){
+        return range;
+    }
+    public int getCapacity(){
+        return capacity;
+    }
+    public int getRemainingFuel(int distance){
+        return range-fuelConsumption*distance;
+    }
 }
