3a976e0e7dc4f6a7580a1a192b38576d32a5dff3
Császár Norbert <csaszar.norbi@gmail.com>
2025-05-25 12:16:55 +0200

    Fleet and Dispatcher classes added

---
+++ src/main/java/org/example/softdevinternassignment/model/Dispatcher.java
@@ -0,0 +1,4 @@
+package org.example.softdevinternassignment.model;
+
+public class Dispatcher {
+}
---
+++ src/main/java/org/example/softdevinternassignment/model/Fleet.java
@@ -0,0 +1,22 @@
+package org.example.softdevinternassignment.model;
+
+import org.example.softdevinternassignment.model.vehicles.VehicleBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fleet {
+    private final List<VehicleBase> vehicles;
+
+    public Fleet(){
+        vehicles= new ArrayList<VehicleBase>();
+    }
+
+
+    public void addVehicle(VehicleBase vehicle){
+        vehicles.add(vehicle);
+    }
+    public List<VehicleBase> getVehicles() {
+        return vehicles;
+    }
+}
