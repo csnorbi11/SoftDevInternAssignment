d8bbaf88ad9229281e5f29b3c529aed0c47b5229
Császár Norbert <csaszar.norbi@gmail.com>
2025-05-25 12:35:59 +0200

    Dispatcher now has suggestVehicle which gives that vehicle which has the highest profit for that trip   calculateProfit now private

--- src/main/java/org/example/softdevinternassignment/model/Dispatcher.java
+++ src/main/java/org/example/softdevinternassignment/model/Dispatcher.java
@@ -4,13 +4,23 @@ import org.example.softdevinternassignment.model.vehicles.VehicleBase;
 
 public class Dispatcher {
 
-    Fleet fleet;
+    private final Fleet fleet;
 
     public Dispatcher() {
         fleet=new Fleet();
     }
 
-    public int calculateProfit(Trip trip, VehicleBase vehicle){
+    public VehicleBase suggestVehicle(Trip trip){
+        VehicleBase bestOption=fleet.getVehicles().getFirst();
+        for(VehicleBase vehicle:fleet.getVehicles()){
+            if(calculateProfit(trip,vehicle)>calculateProfit(trip,bestOption)){
+                bestOption=vehicle;
+            }
+        }
+        return bestOption;
+    }
+
+    private int calculateProfit(Trip trip, VehicleBase vehicle){
         int profit=0;
         int distanceFee=trip.getPassengerCount()* trip.getDistance()*2;
         int timeFee=trip.getTripTime();
