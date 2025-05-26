f59fce40935ace4e142eac187e8b4b3bfd6a54be
Császár Norbert <csaszar.norbi@gmail.com>
2025-05-26 13:06:06 +0200

    HybridVehicle override getRefuelCost()

--- src/main/java/org/example/softdevinternassignment/model/vehicles/HybridVehicle.java
+++ src/main/java/org/example/softdevinternassignment/model/vehicles/HybridVehicle.java
@@ -2,8 +2,18 @@ package org.example.softdevinternassignment.model.vehicles;
 
 public class HybridVehicle extends VehicleBase {
     public HybridVehicle(int capacity, int range) {
-        super(capacity, range, 3,0.5f);
+        super(capacity, range, 3,1.f);
 
+    }
+
+    @Override
+    public int getRefuelCost(int distance) {
+        if(distance<=50){
+            return super.getRefuelCost(distance);
+        }else{
+            int remainingDistance=distance-50;
+            return super.getRefuelCost(50)+Math.round(remainingDistance*0.5f);
+        }
     }
 
     @Override
